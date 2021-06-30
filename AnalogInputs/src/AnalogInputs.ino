18fdd9564d2cf6f22b91d593637c6e83a8b61fd4
vikashkaushik <86675397+vikashkaushik@users.noreply.github.com>
2021-06-29 21:18:15 -0500

    Update AnalogInputs.ino

--- AnalogInputs/src/AnalogInputs.ino
+++ AnalogInputs/src/AnalogInputs.ino
@@ -8,16 +8,18 @@ SYSTEM_MODE(MANUAL);
 SYSTEM_THREAD(ENABLED);
 
 void setup() {
+  // initializes the LED at D5 once as a output in the start
   pinMode(A5,INPUT);
+  // initializes the potentiometer at D5 once as a output in the start
   pinMode(D5,OUTPUT);
     
 }
 
-
 void loop() {
-  
+  // made a variable for value which will be the integer value that the potentiometer is set to 0 to 4095.
   uint16_t value; 
   Serial.begin(9600); 
+  // it will read the value and print it in the console
   value = analogRead(A5);  
   Serial.println(value);  
   digitalWrite(D5,HIGH); // turns it on 
